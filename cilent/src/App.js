0633731a723f037fa9b38012f04a0fa0eb085576
Danny <dannyisapple@gmail.com>
2020-06-04 13:58:44 -0400

    added side navbar

--- cilent/src/App.js
+++ cilent/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/NavBar";
 import "./App.css";
 
 function App() {
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <div className="App">
+      <Sidebar color></Sidebar>
+    </div>
   );
 }
 

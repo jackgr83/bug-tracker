d08035121fa0ddab6431ffc0370dede48ca7c71a
Danny <dannyisapple@gmail.com>
2020-06-09 12:21:42 -0400

    added UI in REACT

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,16 @@ import Button from "@material-ui/core/Button";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/NavBar";
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import Welcome from "./components/Welcome";
+import Login from "./components/Login";
+import Register from "./components/Register";
 function App() {
   return (
     <div className="App">
-      <Sidebar color></Sidebar>
+      {/* <Welcome></Welcome> */}
+      {/* <Login></Login> */}
+      <Register></Register>
     </div>
   );
 }
